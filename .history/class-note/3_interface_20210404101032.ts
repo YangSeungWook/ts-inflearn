cb426a2baa37c242bbac8e4a03c6365d317bfdce
YangSeungWook <kingysw@naver.com>
2021-04-04 18:17:09 +0900

    인터페이스

---
+++ .history/class-note/3_interface_20210404101032.ts
@@ -0,0 +1,10 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+//변수에 활용한 인터페이스
+var seho: User = {
+    age : 33,
+    name : '세호'
+}
---
+++ .history/class-note/3_interface_20210404102253.ts
@@ -0,0 +1,33 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+//변수에 활용한 인터페이스
+var seho: User = {
+    age : 33,
+    name : '세호'
+}
+
+//함수에 인터페이스 활용
+function getUser(user: User){
+    console.log(user);    
+}
+const capt = {
+    name : 'capt',
+    age : 100
+}
+
+getUser(capt);
+
+//함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a:number, b:number) : number;
+}
+
+var sum: SumFunction;
+
+sum = function(a:number, b:number) : number {
+    return a + b;
+}
+
